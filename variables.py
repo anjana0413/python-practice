4cd89c27f07fa12a22b5d0e63daf4f70eb4ad791
anjana0413 <anjanapramanik000.com>
2026-07-09 13:12:50 +0530

    Intial commit

---
+++ variables.py
@@ -0,0 +1,13 @@
+name= "Anjana Kumari"
+experience = 4.8
+domain = "Avionics Domain"
+designation = "Test and Verification & Validation Engineer"
+salary = 790000.00
+is_working = True
+
+print ("Name:", name)
+print ("Experience:",experience)
+print ("Domain:", domain)
+print ("Designation:", designation)
+print ("Salary:",salary)
+print ("Working:",is_working)
