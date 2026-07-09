e5ab6eaee63bb0889651f16b56ea246719f1a6d3
anjana0413 <anjanapramanik000.com>
2026-07-09 15:05:46 +0530

    Added python data types examples

---
+++ variables/data_types.py
@@ -0,0 +1,18 @@
+#String 
+name= "Anjana Kumari"
+#Integer
+age = 32
+#Float
+experience = 4.8
+#Boolean
+is_working = True
+
+print ("Name:", name)
+print ("Age:", age)
+print ("Experience:",experience)
+print ("Working:",is_working)
+ 
+print(type(name))
+print(type(age))
+print(type(experience))
+print(type(is_working))
