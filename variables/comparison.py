743a1fc1c40fffb9fa4f4346650328737922355f
anjana0413 <anjanapramanik000.com>
2026-07-14 11:09:10 +0530

    Added comparison operator

---
+++ variables/comparison.py
@@ -0,0 +1,20 @@
+""" a=20
+b=15
+
+print("a == b",a == b)
+print ("a != b",a != b)
+print ("a>b",a>b)
+print ("a<b",a<b)
+print ("a>=b",a>=b)
+print ("a<=b",a<=b)
+
+"""
+
+
+
+age = 27
+
+print (age >18)
+print (age == 27)
+print (age!=30)
+print (age<= 40)
